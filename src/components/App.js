65b9f21c9a99835464a1b32e4fc40ec30340882a
Chad Pjontek <chadpjontek@gmail.com>
2019-06-09 11:25:07 -0400

    feat: Lazy load routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import SignIn from './SignIn';
-import Home from './Home';
-import CreateList from './CreateList';
-import ViewLists from './ViewLists';
-import Donate from './Donate';
-import EditList from './EditList';
-import NoMatch from './NoMatch';
+
+import Loading from './Loading';
+
 import '../styles/App.scss';
 import 'react-toastify/dist/ReactToastify.min.css';
+
+// lazy load routes
+const Home = lazy(() => import(/* webpackChunkName: "Home" */'./Home'));
+const SignIn = lazy(() => import(/* webpackChunkName: "SignIn" */'./SignIn'));
+const CreateList = lazy(() => import(/* webpackChunkName: "CreateList" */'./CreateList'));
+const ViewLists = lazy(() => import(/* webpackChunkName: "ViewLists" */'./ViewLists'));
+const Donate = lazy(() => import(/* webpackChunkName: "Donate" */'./Donate'));
+const EditList = lazy(() => import(/* webpackChunkName: "EditList" */'./EditList'));
+const NoMatch = lazy(() => import(/* webpackChunkName: "NoMatch" */'./NoMatch'));
+
 
 const App = () => {
 
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path='/signin' component={SignIn} />
-        <Route exact path="/lists/create" component={CreateList} />
-        <Route exact path="/lists" component={ViewLists} />
-        <Route path="/donate" component={Donate} />
-        <Route path="/lists/:name/edit" component={EditList} />
-        {/* when none of the above match, <NoMatch> will be rendered */}
-        <Route component={NoMatch} />
-      </Switch>
-      <ToastContainer />
+      <Suspense fallback={Loading}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path='/signin' component={SignIn} />
+          <Route exact path="/lists/create" component={CreateList} />
+          <Route exact path="/lists" component={ViewLists} />
+          <Route path="/donate" component={Donate} />
+          <Route path="/lists/:name/edit" component={EditList} />
+          {/* when none of the above match, <NoMatch> will be rendered */}
+          <Route component={NoMatch} />
+        </Switch>
+        <ToastContainer />
+      </Suspense>
     </div>
   );
 };
---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Loading = () => {
+  return (
+    <div className='container--app'>
+
+    </div>
+  );
+};
+
+export default Loading;
