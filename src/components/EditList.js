10bdaa7d403327667cf8221f72301e5498eb99d5
Chad Pjontek <chadpjontek@gmail.com>
2019-07-08 14:05:38 -0400

    fix: Correct delete btn function deleting items with duplicate names

--- src/components/EditList.js
+++ src/components/EditList.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import uuid from 'uuid/v1';
 import { useStateValue } from '../hooks/stateManager';
 import del from '../images/delete.svg';
 
 // The input form
 const InputForm = (props) => {
-  const { handleSubmit, handleChange, inputContent } = props;
+  const { addItem, handleChange, inputContent } = props;
   return (
-    <form onSubmit={handleSubmit}>
-      <input id='input' type='text' required onChange={handleChange} />
+    <form onSubmit={addItem}>
+      <input id='input' type='text' required onChange={handleChange} value={inputContent} />
       <span className='bar'></span>
       <label htmlFor='input'>Add item</label>
     </form>
@@ -16,21 +17,18 @@ const InputForm = (props) => {
 
 // Item
 const Item = (props) => {
-  const { content, deleteItem } = props;
-  // Calculate the approx length of content in px
-  const itemWidth = content.length * 9.24;
+  const { item, deleteItem } = props;
+  console.log(props);
   // Calculate the available screen space to add item
   const screenSpace = window.innerWidth - 80;
-  // Calculate lines needed to add the item
-  const linesNeeded = Math.ceil(itemWidth / screenSpace);
   // Create regex to determine where to split string into array
   const re = new RegExp(`.{1,${Math.floor(screenSpace / 9.24)}}`, 'g');
   // Split the string into an array
-  const lines = content.match(re);
+  const lines = item.content.match(re);
   return (
     <div>
-      {lines.map((e, i) => <li key={'line-' + i} className="item">{e}</li>)}
-      <button onClick={deleteItem} className='btn-del'><img src={del} alt="delete" /></button>
+      {lines.map((line) => <li key={uuid()} className="item">{line}</li>)}
+      <button onClick={() => deleteItem(item.id)} className='btn-del'><img src={del} alt="delete" /></button>
     </div>
   );
 };
@@ -64,12 +62,10 @@ const EditList = (props) => {
   const [items, setItems] = useState([]);
 
 
-  // Handle form submission
-  const handleSubmit = e => {
+  // Add item to list
+  const addItem = e => {
     e.preventDefault();
-    // TODO: add an li with inputContext and a delete button to the right
-    const values = [...items];
-    setItems(values.concat(inputContent));
+    setItems([...items, { id: uuid(), content: inputContent }]);
   };
 
   // Handle text input changes
@@ -78,8 +74,8 @@ const EditList = (props) => {
   };
 
   // Delete item
-  const deleteItem = e => {
-    const itemList = items.filter(item => item !== e);
+  const deleteItem = id => {
+    const itemList = items.filter(item => item.id !== id);
     setItems(itemList);
   };
 
@@ -87,11 +83,11 @@ const EditList = (props) => {
     <div className='container-edit'>
       <h1 className='h1'>{listName}</h1>
       <InputForm
-        handleSubmit={handleSubmit}
+        addItem={addItem}
         handleChange={handleChange}
         inputContent={inputContent} />
       <ul>
-        {items.map((e, i) => <Item key={'item-' + i} content={e} deleteItem={() => deleteItem(e)} />)}
+        {items.map((item) => <Item key={item.id} item={item} deleteItem={() => deleteItem(item.id)} />)}
       </ul>
     </div>
   );
